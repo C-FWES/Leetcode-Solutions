6138fddd8beaad32c8f69879b13975c4fc82de1b
Wesley <69126784+C-FWES@users.noreply.github.com>
2021-03-28 17:13:12 -0400

    MergeSorterArray

---
+++ MergeSortedArray.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1, 2, 3, 0, 0, 0};
+        int[] nums2 = new int[]{2, 5, 6};
+        int m = 3;
+        int n = 3;
+        merge(nums1, m, nums2, n);
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = m; i < nums1.length; i++) {
+            nums1[i] = nums2[i-m];
+        }
+        Arrays.sort(nums1);
+    }
+}
